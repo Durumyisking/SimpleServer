76c86bb90fb4c583dbc4cf7d0752106917cdd1f2
durumyisking <cww9512@gmail.com>
2023-06-28 15:59:02 +0900

    sever local/ip 주소 확인 코드 추가 \n 오류 : client 측에서 연결 시도할때 포트가 다르게 적용됨

--- server/server/server.cpp
+++ server/server/server.cpp
@@ -15,10 +15,10 @@ void HandleClient(SOCKET clientSocket);
 int main()
 {
     WSADATA wsd;
-    SOCKET sListen;
-    std::vector<SOCKET> clientSockets;
+    SOCKET serverSockets; // 서버소켓 : 클라이언트 연결 수락 및 클라이언트 소켓 생성 담당  
+    std::vector<SOCKET> clientSockets; // 클라이언트 소켓들을 담는 벡터
 
-    struct sockaddr_in local;
+    struct sockaddr_in localAddr; // 서버소켓이 바인딩 될 로컬주소
 
     if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0)
     {
@@ -29,8 +29,8 @@ int main()
         puts("The library has been initialized.\n\n");
     }
 
-    sListen = socket(AF_INET, SOCK_STREAM, 0);
-    if (sListen == INVALID_SOCKET)
+    serverSockets = socket(AF_INET, SOCK_STREAM, 0); // 서버 소켓 생성
+    if (serverSockets == INVALID_SOCKET)
     {
         ErrorHandling(L"socket() error!");
     }
@@ -39,24 +39,63 @@ int main()
         std::cout << "Server socket created.\n" << std::endl;
     }
 
-    memset(&local, 0, sizeof(local));
-    local.sin_addr.s_addr = htonl(INADDR_ANY);
-    local.sin_family = AF_INET;
-    local.sin_port = htons(DEFAULT_PORT);
+    memset(&localAddr, 0, sizeof(localAddr));
+    localAddr.sin_addr.s_addr = htonl(INADDR_ANY); // 아이피 주소 설정 (INADDR_ANY 키워드로 모든 네트워크 인터페이스에서 들어오는 연결을 수락)
+    localAddr.sin_port = htons(DEFAULT_PORT); // 포트넘버 설정
+    localAddr.sin_family = AF_INET; // IPv4로 주소 체계 설정
 
-    if (bind(sListen, (struct sockaddr*)&local, sizeof(local)) == SOCKET_ERROR)
+    if (bind(serverSockets, (struct sockaddr*)&localAddr, sizeof(localAddr)) == SOCKET_ERROR)
     {
         ErrorHandling(L"bind() error!");
     }
 
-    listen(sListen, SOMAXCONN);
+    listen(serverSockets, SOMAXCONN);
+
+    char serverIP[20] = { 0 };
+    if (NULL == inet_ntop(AF_INET, &localAddr.sin_addr, serverIP, sizeof(serverIP))) // inet_ntop ip 주소를 문자열 형태로 변환
+    {
+        ErrorHandling(L"inet_ntop() error!");
+    }
+    else
+    {
+        std::cout << "[Server IP: " << serverIP << "::" << ntohs(localAddr.sin_port) << "]" << std::endl;
+    }
+
+    char hostname[256];
+    if (gethostname(hostname, sizeof(hostname)) != 0) {
+        std::cout << "Failed to get hostname" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    struct addrinfo* ipAddr = nullptr;
+    struct addrinfo hints {};
+    hints.ai_family = AF_INET;
+
+    if (getaddrinfo(hostname, nullptr, &hints, &ipAddr) != 0) {
+        std::cout << "Failed to get IP address" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    struct sockaddr_in* addr = reinterpret_cast<struct sockaddr_in*>(ipAddr->ai_addr);
+    char ipAddress[INET_ADDRSTRLEN];
+    if (inet_ntop(AF_INET, &(addr->sin_addr), ipAddress, INET_ADDRSTRLEN) == nullptr) {
+        std::cout << "Failed to convert IP address to string" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "IP address: " << ipAddress << std::endl;
+
+    freeaddrinfo(ipAddr);
 
     while (1)
     {
         struct sockaddr_in clientAddr;
         int clientAddrSize = sizeof(clientAddr);
 
-        SOCKET clientSocket = accept(sListen, (struct sockaddr*)&clientAddr, &clientAddrSize);
+        SOCKET clientSocket = accept(serverSockets, (struct sockaddr*)&clientAddr, &clientAddrSize);
 
         if (clientSocket == INVALID_SOCKET)
         {
@@ -71,7 +110,7 @@ int main()
             }
             else
             {
-                std::cout << "[Client IP: " << clientIP << ", PORT:" << ntohs(clientAddr.sin_port) << "]" << std::endl;
+                std::cout << "[Client IP: " << clientIP << "::" << ntohs(clientAddr.sin_port) << "]" << std::endl;
 //                printf("[Client IP:%s, PORT: %d\n", clientIP, ntohs(clientAddr.sin_port)); // ntohs 포트넘버를 정수 형태로 변환
                 clientSockets.push_back(clientSocket);
 
@@ -87,7 +126,7 @@ int main()
         closesocket(clientSocket);
     }
 
-    closesocket(sListen);
+    closesocket(serverSockets);
 
     WSACleanup();
 
